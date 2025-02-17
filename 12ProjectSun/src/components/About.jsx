30a517b0f450dc0efab3a1ee4fe1e6b838d43189
Chinmayindoria <indoriachinmay44@gmail.com>
2025-02-17 20:00:27 +0530

    ..

--- 12ProjectSun/src/components/About.jsx
+++ 12ProjectSun/src/components/About.jsx
@@ -32,35 +32,50 @@ export default function About() {
 
 
 
-    <div className = "h-4/5 w-4/5 absolute left-36 top-32  z-10">
-        <div className="py-16 ">
-          <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
-              <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-                  <div className="md:5/12 lg:w-5/12">
-                       <img
-                          className="object-cover w-full h-96 z-10 rounded-md"
-                          src="Apj.webp"
-                          alt="image"
-                      />
-                  </div>
-                  <div className="md:7/12 lg:w-6/12">
-                      <h2 className="text-2xl text-white font-bold md:text-4xl">
-                          React development is carried out by passionate developers
-                      </h2>
-                      <p className="mt-6 text-white">
-                          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem
-                          accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde
-                          aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!
-                      </p>
-                      <p className="mt-4 text-white">
-                          Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at?
-                          Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.
-                      </p>
-                  </div>
-              </div>
-          </div>
-      </div>
+    // <div className = "h-4/5 w-4/5 absolute left-36 top-32  z-10">
+    //     <div className="py-16 ">
+    //       <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+    //           <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+    //               <div className="md:5/12 lg:w-5/12">
+    //                    <img
+    //                       className="object-cover w-full h-96 z-10 rounded-md"
+    //                       src="Apj.webp"
+    //                       alt="image"
+    //                   />
+    //               </div>
+    //               <div className="md:7/12 lg:w-6/12">
+    //                   <h2 className="text-2xl text-white font-bold md:text-4xl">
+    //                       React development is carried out by passionate developers
+    //                   </h2>
+    //                   <p className="mt-6 text-white">
+    //                       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem
+    //                       accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde
+    //                       aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!
+    //                   </p>
+    //                   <p className="mt-4 text-white">
+    //                       Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at?
+    //                       Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.
+    //                   </p>
+    //               </div>
+    //           </div>
+    //       </div>
+    //   </div>
+    // </div>
+
+    <div>
+        <div className=" text-3xl text-orange-300  h-10 w-3/12 m-5  mt-20 absolute right-1/4 -translate-x-5 font-bold">
+    <h1>About Us</h1>
+  </div>
+  <div className='text-2xl text-black  w-1/2 h-4/6 m-5 mt-32 right-5 absolute font-medium'>
+    <p>Space Technology and Education Pvt. Ltd., also recognized as SPACE India, is a pioneer organization with a legacy of 24 years in developing and popularizing Astronomy, Space Science, and STEM education and experiences at Grassroots level (K-12) and beyond. As a registered Space Tutor of the Indian Space Research Organization (ISRO), SPACE India has been instrumental in creating an impact through science, and aligns with the United Nations’ Sustainable Development Goals.
+
+      SPACE India has inspired 1.5M+ Students and empowered 1,000+ Schools through a diverse range of products and services. SPACE India is the parent company of the SPACE Group of companies.</p>
+  </div>
+  <div className=" text-3xl text-white bg-red-400 h-40 w-4/12 m-5 mt-20 rounded-lg absolute left-40 ">
+    <img src="https://space-india.com/wp-content/uploads/2024/04/Cover-Images-with-Logo-2048x1729.png" alt="" />
+  </div>
     </div>
+
  
   );
 }
